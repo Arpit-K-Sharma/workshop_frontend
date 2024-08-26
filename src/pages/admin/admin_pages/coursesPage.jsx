4809c8b33b87830e274e1e1748b7f434a3862753
Sarthak Shrestha <sarthakstha24@gmail.com>
2024-08-26 12:37:08 +0545

    Text Changes

--- src/pages/admin/admin_pages/coursesPage.jsx
+++ src/pages/admin/admin_pages/coursesPage.jsx
@@ -1,25 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import AdminSidebar from '../adminSidebar';
-import axios from 'axios';
-import { Button } from '@/components/ui/button';
-import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter } from '@/components/ui/dialog';
-import { Input } from '@/components/ui/input';
-import { ChevronDown, ChevronUp, Code, Book, Clock } from 'lucide-react'; 
-import javascript from '../../../gallery/images/javascript.png';
-import python from '../../../gallery/images/PythonProgramming.jpg';
+import React, { useState, useEffect } from "react";
+import AdminSidebar from "../adminSidebar";
+import axios from "axios";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogFooter,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { ChevronDown, ChevronUp, Code, Book, Clock } from "lucide-react";
+import javascript from "../../../gallery/images/javascript.png";
+import python from "../../../gallery/images/PythonProgramming.jpg";
+import { baseURL } from "@/utils/axiosInstance";
 
 const CourseCard = ({ course }) => {
   const [showDescription, setShowDescription] = useState(false);
 
   const getCourseImage = (courseName) => {
-    if (courseName.toLowerCase().includes('javascript')) {
+    if (courseName.toLowerCase().includes("javascript")) {
       return javascript;
     }
     return python;
   };
 
   return (
-    <div className={`bg-white rounded-lg shadow-lg overflow-hidden transform transition duration-300 hover:scale-105 hover:shadow-xl min-w-[300px] ${showDescription ? 'h-auto' : 'h-[335px]'}`}>
+    <div
+      className={`bg-white rounded-lg shadow-lg overflow-hidden transform transition duration-300 hover:scale-105 hover:shadow-xl min-w-[300px] ${
+        showDescription ? "h-auto" : "h-[335px]"
+      }`}
+    >
       <img
         src={getCourseImage(course.course_name)}
         alt={course.course_name}
@@ -27,20 +40,27 @@ const CourseCard = ({ course }) => {
       />
       <div className="p-4">
         <h2 className="text-2xl font-bold mb-2 text-gray-800 flex items-center">
-          <Code className="mr-2"  size={24} />
+          <Code className="mr-2" size={24} />
           {course.course_name}
         </h2>
-        <h3 style={{ letterSpacing: "1.5px" }} className="text-lg font-semibold mb-2 text-gray-600 flex items-center">
-          <Book className="mr-2"  size={20} />
+        <h3
+          style={{ letterSpacing: "1.5px" }}
+          className="text-lg font-semibold mb-2 text-gray-600 flex items-center"
+        >
+          <Book className="mr-2" size={20} />
           {course.course_content}
         </h3>
         {showDescription && (
           <p className="text-gray-500 mb-4 ">--- {course.description}</p>
         )}
         <div className="flex justify-between items-center mt-4">
-          <span className="text-sm font-medium text-gray-800 flex items-center" style={{ letterSpacing: "1px" }}>
+          <span
+            className="text-sm font-medium text-gray-800 flex items-center"
+            style={{ letterSpacing: "1px" }}
+          >
             <Clock className="mr-1" color="black" size={23} />
-            Duration: <span className="text-gray-800 ml-1">{course.course_duration}</span>
+            Duration:{" "}
+            <span className="text-gray-800 ml-1">{course.course_duration}</span>
           </span>
           <Button
             onClick={() => setShowDescription(!showDescription)}
@@ -67,19 +87,19 @@ const CourseCard = ({ course }) => {
 const CoursesPage = () => {
   const [courses, setCourses] = useState([]);
   const [newCourse, setNewCourse] = useState({
-    course_name: '',
-    course_content: '',
-    course_duration: '',
-    description: ''
+    course_name: "",
+    course_content: "",
+    course_duration: "",
+    description: "",
   });
 
   useEffect(() => {
     const fetchCourses = async () => {
       try {
-        const response = await axios.get('http://localhost:8000/course');
+        const response = await axios.get(`${baseURL}/course`);
         setCourses(response.data.data);
       } catch (error) {
-        console.error('Error fetching courses:', error);
+        console.error("Error fetching courses:", error);
       }
     };
 
@@ -90,17 +110,20 @@ const CoursesPage = () => {
     const { name, value } = e.target;
     setNewCourse((prevState) => ({
       ...prevState,
-      [name]: value
+      [name]: value,
     }));
   };
 
   const handleAddCourse = async () => {
     try {
       // Replace the below URL with your backend endpoint for adding courses
-      const response = await axios.post('http://localhost:8000/course', newCourse);
+      const response = await axios.post(
+        "http://localhost:8000/course",
+        newCourse
+      );
       setCourses([...courses, response.data.data]);
     } catch (error) {
-      console.error('Error adding course:', error);
+      console.error("Error adding course:", error);
     }
   };
 
@@ -108,7 +131,9 @@ const CoursesPage = () => {
     <div className="flex bg-gray-100 min-h-screen">
       <AdminSidebar />
       <div className="flex-1 p-10 ml-[220px] xl:ml-[270px]">
-        <h1 className="text-4xl font-bold mb-8 text-center text-gray-800">Courses</h1>
+        <h1 className="text-4xl font-bold mb-8 text-center text-gray-800">
+          Courses
+        </h1>
         <div className="mb-4">
           <Dialog>
             <DialogTrigger asChild>
@@ -150,7 +175,10 @@ const CoursesPage = () => {
                 />
               </div>
               <DialogFooter>
-                <Button onClick={handleAddCourse} className="bg-homeText w-full hover:bg-[#434311]">
+                <Button
+                  onClick={handleAddCourse}
+                  className="bg-homeText w-full hover:bg-[#434311]"
+                >
                   Submit
                 </Button>
               </DialogFooter>
@@ -158,9 +186,10 @@ const CoursesPage = () => {
           </Dialog>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 xl:gap-20">
-          {courses.length > 0 && courses.map((course, index) => (
-            <CourseCard key={index} course={course} />
-          ))}
+          {courses.length > 0 &&
+            courses.map((course, index) => (
+              <CourseCard key={index} course={course} />
+            ))}
         </div>
       </div>
     </div>
--- src/pages/admin/admin_pages/mentorsPage.jsx
+++ src/pages/admin/admin_pages/mentorsPage.jsx
@@ -1,20 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import AdminSidebar from '../adminSidebar';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import axios from 'axios';
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter } from '@/components/ui/dialog';
-import { Trash, Plus, X, Check } from 'lucide-react';
-import { Table, TableHead, TableHeader, TableBody, TableRow, TableCell } from '@/components/ui/table'; 
+import React, { useEffect, useState } from "react";
+import AdminSidebar from "../adminSidebar";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import axios from "axios";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogFooter,
+} from "@/components/ui/dialog";
+import { Trash, Plus, X, Check } from "lucide-react";
+import {
+  Table,
+  TableHead,
+  TableHeader,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@/components/ui/table";
+import { baseURL } from "@/utils/axiosInstance";
 
 const MentorsPage = () => {
   const [mentors, setMentors] = useState([]);
   const [newMentor, setNewMentor] = useState({
-    name: '',
-    address: '',
-    username: '',
-    password: '',
-    phone_num: '',
+    name: "",
+    address: "",
+    username: "",
+    password: "",
+    phone_num: "",
     profile_pic: null,
   });
   const [isDialogOpen, setIsDialogOpen] = useState(false);
@@ -27,19 +42,19 @@ const MentorsPage = () => {
 
   const fetchMentors = async () => {
     try {
-      const response = await axios.get('http://localhost:8000/teacher');
+      const response = await axios.get(`${baseURL}/teacher`);
       setMentors(response.data.data);
     } catch (error) {
-      console.error('Error fetching mentors:', error);
+      console.error("Error fetching mentors:", error);
     }
   };
 
   const handleDelete = async (id) => {
     try {
-      await axios.delete(`http://localhost:8000/teacher/${id}`);
+      await axios.delete(`${baseURL}/teacher/${id}`);
       fetchMentors();
     } catch (error) {
-      console.error('Error deleting mentor:', error);
+      console.error("Error deleting mentor:", error);
     }
   };
 
@@ -50,14 +65,21 @@ const MentorsPage = () => {
     });
 
     try {
-      await axios.post('http://localhost:8000/teacher', formData, {
-        headers: { 'Content-Type': 'multipart/form-data' }
+      await axios.post(`${baseURL}/teacher`, formData, {
+        headers: { "Content-Type": "multipart/form-data" },
       });
       fetchMentors();
-      setNewMentor({ name: '', address: '', username: '', password: '', phone_num: '', profile_pic: null });
+      setNewMentor({
+        name: "",
+        address: "",
+        username: "",
+        password: "",
+        phone_num: "",
+        profile_pic: null,
+      });
       setIsDialogOpen(false);
     } catch (error) {
-      console.error('Error creating mentor:', error);
+      console.error("Error creating mentor:", error);
     }
   };
 
@@ -77,78 +99,174 @@ const MentorsPage = () => {
   return (
     <div>
       <AdminSidebar />
-      <div className='ml-[220px]'>
-        <h1 className='text-4xl pt-[40px] font-bold mb-4 text-center mr-[5%]'>Mentors</h1>
-        <Button onClick={handleOpenDialog} className="mb-4 float-right mr-[4%] bg-homeText hover:bg-homeText-hover"><Plus /> Add Mentor</Button>
+      <div className="ml-[220px]">
+        <h1 className="text-4xl pt-[40px] font-bold mb-4 text-center mr-[5%]">
+          Mentors
+        </h1>
+        <Button
+          onClick={handleOpenDialog}
+          className="mb-4 float-right mr-[4%] text-white bg-zinc-800 hover:bg-zinc-700"
+        >
+          <Plus /> Add Mentor
+        </Button>
 
         <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-          <DialogContent>
+          <DialogContent className="bg-white">
             <DialogHeader>
               <DialogTitle>Add New Mentor</DialogTitle>
             </DialogHeader>
             <div className="p-4 space-y-4">
-              <Input placeholder="Name" value={newMentor.name} onChange={(e) => setNewMentor({ ...newMentor, name: e.target.value })} />
-              <Input placeholder="Address" value={newMentor.address} onChange={(e) => setNewMentor({ ...newMentor, address: e.target.value })} />
-              <Input placeholder="Username" value={newMentor.username} onChange={(e) => setNewMentor({ ...newMentor, username: e.target.value })} />
-              <Input placeholder="Password" type="password" value={newMentor.password} onChange={(e) => setNewMentor({ ...newMentor, password: e.target.value })} />
-              <Input placeholder="Phone Number" value={newMentor.phone_num} onChange={(e) => setNewMentor({ ...newMentor, phone_num: e.target.value })} />
-              <Input type="file" onChange={(e) => setNewMentor({ ...newMentor, profile_pic: e.target.files[0] })} />
+              <Input
+                placeholder="Name"
+                value={newMentor.name}
+                onChange={(e) =>
+                  setNewMentor({ ...newMentor, name: e.target.value })
+                }
+              />
+              <Input
+                placeholder="Address"
+                value={newMentor.address}
+                onChange={(e) =>
+                  setNewMentor({ ...newMentor, address: e.target.value })
+                }
+              />
+              <Input
+                placeholder="Username"
+                value={newMentor.username}
+                onChange={(e) =>
+                  setNewMentor({ ...newMentor, username: e.target.value })
+                }
+              />
+              <Input
+                placeholder="Password"
+                type="password"
+                value={newMentor.password}
+                onChange={(e) =>
+                  setNewMentor({ ...newMentor, password: e.target.value })
+                }
+              />
+              <Input
+                placeholder="Phone Number"
+                value={newMentor.phone_num}
+                onChange={(e) =>
+                  setNewMentor({ ...newMentor, phone_num: e.target.value })
+                }
+              />
+              <Input
+                type="file"
+                onChange={(e) =>
+                  setNewMentor({ ...newMentor, profile_pic: e.target.files[0] })
+                }
+              />
             </div>
             <DialogFooter>
-              <Button className="bg-homeText hover:bg-homeText-hover" onClick={handleCreate}><Check /> Save</Button>
-              <Button className="bg-homeText hover:bg-homeText-hover" onClick={handleCloseDialog}><X /> Cancel</Button>
+              <Button
+                className="bg-homeText hover:bg-homeText-hover"
+                onClick={handleCreate}
+              >
+                <Check /> Save
+              </Button>
+              <Button
+                className="bg-homeText hover:bg-homeText-hover"
+                onClick={handleCloseDialog}
+              >
+                <X /> Cancel
+              </Button>
             </DialogFooter>
           </DialogContent>
         </Dialog>
 
         <Dialog open={isDeleteDialogOpen} onOpenChange={setIsDeleteDialogOpen}>
-          <DialogContent>
+          <DialogContent className="bg-white">
             <DialogHeader>
               <DialogTitle>Confirm Deletion</DialogTitle>
             </DialogHeader>
-            <DialogDescription>Are you sure you want to delete this mentor?</DialogDescription>
+            <DialogDescription>
+              Are you sure you want to delete this mentor?
+            </DialogDescription>
             <DialogFooter>
-              <Button className="bg-green-800 hover:bg-green-700" onClick={confirmDelete}><Check /> Yes</Button>
-              <Button className="bg-red-800 hover:bg-red-700" onClick={handleCloseDeleteDialog}><X /> No</Button>
+              <Button
+                className="bg-green-800 hover:bg-green-700 text-white"
+                onClick={confirmDelete}
+              >
+                <Check /> Yes
+              </Button>
+              <Button
+                className="bg-red-800 hover:bg-red-700 text-white"
+                onClick={handleCloseDeleteDialog}
+              >
+                <X /> No
+              </Button>
             </DialogFooter>
           </DialogContent>
         </Dialog>
 
-        <div className='pr-4 pl-4'>
+        <div className="pr-4 pl-4">
           {mentors.length > 0 ? (
             <Table className="min-w-full ">
-              <TableHeader className='bg-gray-100'>
+              <TableHeader className="bg-gray-100">
                 <TableRow>
-                  <TableHead className='text-center font-bold '>Name</TableHead>
-                  <TableHead className='text-center font-bold'>Address</TableHead>
-                  <TableHead className='text-center font-bold'>Username</TableHead>
-                  <TableHead className='text-center font-bold'>Phone Number</TableHead>
-                  <TableHead className='text-center font-bold'>Profile Pic</TableHead>
-                  <TableHead className='text-center font-bold'>Actions</TableHead>
+                  <TableHead className="text-center font-bold ">Name</TableHead>
+                  <TableHead className="text-center font-bold">
+                    Address
+                  </TableHead>
+                  <TableHead className="text-center font-bold">
+                    Username
+                  </TableHead>
+                  <TableHead className="text-center font-bold">
+                    Phone Number
+                  </TableHead>
+                  <TableHead className="text-center font-bold">
+                    Profile Pic
+                  </TableHead>
+                  <TableHead className="text-center font-bold">
+                    Actions
+                  </TableHead>
                 </TableRow>
               </TableHeader>
               <TableBody>
-                {mentors.map(mentor => (
-                  <TableRow key={mentor.id} className='text-[#2d2c2c]'>
-                    <TableCell className='text-center font-semibold '  >{mentor.name}</TableCell>
-                    <TableCell className='text-center font-semibold'>{mentor.address}</TableCell>
-                    <TableCell className='text-center font-semibold'>{mentor.username}</TableCell>
-                    <TableCell className='text-center font-semibold'>{mentor.phone_num}</TableCell>
-                    <TableCell className='text-center font-semibold'>
+                {mentors.map((mentor) => (
+                  <TableRow key={mentor.id} className="text-[#2d2c2c]">
+                    <TableCell className="text-center font-semibold ">
+                      {mentor.name}
+                    </TableCell>
+                    <TableCell className="text-center font-semibold">
+                      {mentor.address}
+                    </TableCell>
+                    <TableCell className="text-center font-semibold">
+                      {mentor.username}
+                    </TableCell>
+                    <TableCell className="text-center font-semibold">
+                      {mentor.phone_num}
+                    </TableCell>
+                    <TableCell className="text-center font-semibold">
                       {mentor.profile_pic ? (
-                        <img src={mentor.profile_pic} alt="Profile" className="w-10 h-10 rounded-full" />
+                        <img
+                          src={mentor.profile_pic}
+                          alt="Profile"
+                          className="w-10 h-10 rounded-full"
+                        />
                       ) : (
-                        'No Image'
+                        "No Image"
                       )}
                     </TableCell>
-                    <TableCell className='text-center font-semibold'>
-                      <Button className="bg-red-800 hover:bg-red-700" onClick={() => { handleOpenDeleteDialog(mentor.id); }}><Trash /> Delete</Button>
+                    <TableCell className="text-center font-semibold">
+                      <Button
+                        className="bg-red-800 hover:bg-red-700 text-white"
+                        onClick={() => {
+                          handleOpenDeleteDialog(mentor.id);
+                        }}
+                      >
+                        <Trash /> Delete
+                      </Button>
                     </TableCell>
                   </TableRow>
                 ))}
               </TableBody>
             </Table>
-          ) : <p>No mentors found</p>}
+          ) : (
+            <p>No mentors found</p>
+          )}
         </div>
       </div>
     </div>
