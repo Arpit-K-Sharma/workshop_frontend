f39d895795b1ac14e486b63aa9afd7f9d8f670d9
Sarthak Shrestha <sarthakstha24@gmail.com>
2024-09-05 18:54:08 +0545

    Protecting School Routes

--- src/pages/authentication/util.jsx
+++ src/pages/authentication/util.jsx
@@ -4,7 +4,7 @@ import CryptoJS from "crypto-js";
 
 // Function to get the role from the token
 export const getRoleFromToken = () => {
-  const SECRET_KEY = "MRgnS4LVB8SvJWu1JexdRGlCOCrKfBQ9";
+  const SECRET_KEY = process.env.REACT_APP_SECRET_KEY;
   const encryptedtoken = Cookies.get("access_token");
   if (encryptedtoken) {
     const bytes = CryptoJS.AES.decrypt(encryptedtoken, SECRET_KEY);
